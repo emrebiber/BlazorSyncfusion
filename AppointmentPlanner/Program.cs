1744c10fa749f27b4ce08fbb6594edd4cf3f2d91
Emre Biber <emrebiber@gmail.com>
2022-10-06 16:08:03 +0300

    Add project files.

---
+++ AppointmentPlanner/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
+using AppointmentPlanner.Data;
+using AppointmentPlanner.Models;
+
+namespace AppointmentPlanner
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddSyncfusionBlazor();
+            builder.Services.AddSingleton<AppointmentService, AppointmentService>();
+            builder.Services.AddSingleton<Appointment, Appointment>();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
